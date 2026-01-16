da52211df1170ec9257f6744bc349efb8927c589
domanskey <domadam04@gmail.com>
2026-01-16 13:51:03 +0100

    feat: add save and get cookie function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
 import './style.css'
+
+const cookieConsent = document.getElementById('cookie');
+const closeButton = document.getElementById('close');
+const acceptButton = document.getElementById('accept');
+
+closeButton.addEventListener('click', () => {
+  cookieConsent.close();
+});
+
+acceptButton.addEventListener('click', () => {
+    document.cookie = "cookies=accepted";
+    cookieConsent.close();
+})
+
+if (getCookie("cookies") !== "accepted")
+setTimeout(() => {
+    cookieConsent.showModal();
+}, 1000);
+
+function getCookie(cookieName) {
+  let name = cookieName + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
